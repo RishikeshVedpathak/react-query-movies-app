a9bd988b41d9d322fac93c4aba00df5231828c63
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-04-26 23:44:11 +0530

    setup react-query client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import Layout from "components/Layout";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Layout />
+      </div>
+    </QueryClientProvider>
   );
 }
 
