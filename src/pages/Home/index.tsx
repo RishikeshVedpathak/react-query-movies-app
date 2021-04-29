19fede26bdaa7871e5619c74a6207ad368a40cfb
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-04-29 12:06:52 +0530

    filter debounce

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect, ReactElement } from "react";
 import styles from "./index.module.css";
 import SearchBox from "components/SearchBox";
 
-const Home = () => {
+const Home = (): ReactElement => {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const filterTimer = setTimeout(() => {
+      console.log(searchText);
+    }, 1000);
+    return () => {
+      clearTimeout(filterTimer);
+    };
+  }, [searchText]);
+
   const handleSearchChange = (text: string) => {
-    console.log(text);
+    setSearchText(text);
   };
 
   return (
