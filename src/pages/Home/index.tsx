33e419ae9712cb2a58d863a1a67c7e40a468ff7d
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-04-30 23:56:41 +0530

    fetch sample data for testing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, ReactElement } from "react";
 import styles from "./index.module.css";
 import SearchBox from "components/SearchBox";
+import service from "utils/service";
+import CONSTANTS from "utils/constants";
 
 const Home = (): ReactElement => {
   const [searchText, setSearchText] = useState("");
@@ -18,9 +20,19 @@ const Home = (): ReactElement => {
     setSearchText(text);
   };
 
+  const fetchData = async () => {
+    const response = await service.get(CONSTANTS.BASE_URL);
+    console.log(response);
+
+    // fetch("http://www.omdbapi.com/?i=tt3896198&apikey=91300fc7")
+    //   .then((response) => response.json())
+    //   .then((response) => console.log(response));
+  };
+
   return (
     <div className={styles.root}>
       <SearchBox onChange={handleSearchChange} />
+      <button onClick={fetchData}>Fetch</button>
     </div>
   );
 };
