ec9080d1a86e51eb4221236d509fcd28cd547211
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-01 01:25:00 +0530

    improved serach box

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from "react";
-import { TextField } from "@material-ui/core";
-import styles from "./index.module.css";
+import { ReactElement, useRef } from "react";
+import { makeStyles, Paper, InputBase, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 type SearchBoxProps = {
   onChange: (text: string) => void;
@@ -9,15 +9,50 @@ const defaultProps = {
   onChange: () => {},
 };
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
 const SearchBox = ({ onChange }: SearchBoxProps): ReactElement => {
+  const classes = useStyles();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleChange = () => {
+    onChange(!!inputRef.current ? inputRef.current.value : "");
+  };
+
   return (
-    <TextField
-      type="text"
-      variant="outlined"
-      placeholder="Search for a movie"
-      size="small"
-      onChange={(event) => onChange(event.target.value)}
-    />
+    <Paper variant="outlined" className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search for a movie"
+        inputRef={inputRef}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            handleChange();
+          }
+        }}
+      />
+      <IconButton
+        type="submit"
+        className={classes.iconButton}
+        onClick={handleChange}
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
