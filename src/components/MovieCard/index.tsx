1137af148abceb42b93172f0849708663d119419
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-02 01:06:10 +0530

    Updated layout and styling

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,24 +1,14 @@
-import { makeStyles } from "@material-ui/core/styles";
+import styles from "./index.module.css";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    height: "100%",
-  },
-  media: {
-    height: 240,
-  },
-});
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 export type MovieCardProps = {
-  //   onChange: (text: string) => void;
   Title: string;
   Year: string;
   imdbID: string;
@@ -27,25 +17,26 @@ export type MovieCardProps = {
 };
 
 const MovieCard = ({ Title, Year, imdbID, Type, Poster }: MovieCardProps) => {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.root}>
+    <Card className={styles.root}>
       <CardActionArea>
-        <CardMedia className={classes.media} image={Poster} title={Title} />
+        <CardMedia
+          component="img"
+          alt={Title}
+          height="200"
+          image={Poster}
+          title={Title}
+        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {Title}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+      <CardActions className={styles.actionsContainer}>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
       </CardActions>
     </Card>
   );
