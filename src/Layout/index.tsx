45d0e2c007b310168cf5808a6656e6d345e75cad
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-05 19:26:19 +0530

    refactor

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "pages/Home";
 import { Paper } from "@material-ui/core";
 
