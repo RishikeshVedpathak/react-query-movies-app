3919a5c69d67639ab2903d4bec075524a5b501a6
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-14 02:19:53 +0530

    WIP

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,52 @@
-import React, { useState, ReactElement } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import styles from "./index.module.css";
 import SearchBox from "components/SearchBox";
 import service from "utils/service";
 import CONSTANTS from "utils/constants";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import MovieCard, { MovieCardProps } from "components/MovieCard";
 import Skeleton from "components/MovieCard/Skeleton";
-import { Grid } from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = (): ReactElement => {
   const [searchText, setSearchText] = useState("man"); // Initial value set to 'man' to display default search results on UI
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    setCurrentPage(0);
+    remove();
+    setTimeout(() => {
+      refetch();
+    }, 1000);
+  }, [searchText]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleSearchChange = (text: string) => {
     setSearchText(text);
   };
 
-  const { isSuccess, isLoading, error, data } = useQuery(
-    [`movies`, searchText],
-    () => service.get(CONSTANTS.BASE_URL, { s: searchText }),
-    {
-      enabled: !!searchText.length,
-    }
-  );
+  const fetchMovies = ({ pageParam = 1 }) =>
+    service.get(CONSTANTS.BASE_URL, {
+      s: searchText,
+      page: pageParam,
+    });
+
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isSuccess,
+    isLoading,
+    refetch,
+    remove,
+  } = useInfiniteQuery(`movies`, fetchMovies, {
+    getNextPageParam: (lastPage, pages) => currentPage + 1,
+    enabled: !!searchText.length,
+    onSuccess: () => {
+      setCurrentPage(currentPage + 1);
+    },
+  });
 
   return (
     <div className={styles.root}>
@@ -48,16 +73,39 @@ const Home = (): ReactElement => {
             )}
 
             {isSuccess &&
-              (!!data && data.Search.length ? (
-                <Grid container spacing={2}>
-                  {data.Search.map(
-                    ({ Title, imdbID, Type, Year, Poster }: MovieCardProps) => (
-                      <Grid item xs={12} md={3} key={imdbID}>
-                        <MovieCard {...{ Title, imdbID, Type, Year, Poster }} />
-                      </Grid>
-                    )
-                  )}
-                </Grid>
+              (!!data ? (
+                <InfiniteScroll
+                  dataLength={
+                    data.pages.reduce((a, b) => {
+                      return { Search: [...a.Search, ...b.Search] };
+                    }).Search.length
+                  }
+                  next={fetchNextPage}
+                  hasMore={hasNextPage || false}
+                  loader={<h4>Loading...</h4>}
+                >
+                  <Grid container spacing={2}>
+                    {data.pages
+                      .reduce((a, b) => {
+                        return { Search: [...a.Search, ...b.Search] };
+                      })
+                      .Search.map(
+                        ({
+                          Title,
+                          imdbID,
+                          Type,
+                          Year,
+                          Poster,
+                        }: MovieCardProps) => (
+                          <Grid item xs={12} md={3} key={imdbID}>
+                            <MovieCard
+                              {...{ Title, imdbID, Type, Year, Poster }}
+                            />
+                          </Grid>
+                        )
+                      )}
+                  </Grid>
+                </InfiniteScroll>
               ) : (
                 "No Result"
               ))}
@@ -66,6 +114,7 @@ const Home = (): ReactElement => {
           </Grid>
         </Grid>
       </Grid>
+      <Button onClick={() => fetchNextPage()}>Fetch more</Button>
     </div>
   );
 };
