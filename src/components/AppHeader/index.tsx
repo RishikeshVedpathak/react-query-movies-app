7a5e21c14d8e151127b38420f2e6408df80c7430
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-05-16 16:01:13 +0530

    Added back navigation arrow in the app header

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,12 +1,25 @@
 import styles from "./index.module.css";
 import { ReactComponent as IMDBLogo } from "assets/images/IMDB_Logo.svg";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosRoundedIcon from "@material-ui/icons/ArrowBackIosRounded";
 
 const AppHeader = (): React.ReactElement => {
   const history = useHistory();
+  const location = useLocation();
 
   return (
-    <div style={{ padding: "0.5rem 2rem" }} className={styles.root}>
+    <div className={styles.root}>
+      <IconButton
+        aria-label="back"
+        onClick={() => history.goBack()}
+        style={{
+          visibility: location.pathname === "/" ? "hidden" : "visible",
+        }}
+        className={styles.backBtn}
+      >
+        <ArrowBackIosRoundedIcon fontSize="large" />
+      </IconButton>
       <IMDBLogo
         height={48}
         width={100}
